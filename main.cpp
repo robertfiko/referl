a61e2caee96529c60dfde9376ed8d3cde7b0f7be
Robert Fiko <fiko.robert@gmail.com>
2021-02-23 00:02:50 +0100

    argc

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-      if (argc == 0)
+      if (argc == 1)
       {
             std::cout << "RefactorErl says hello to Homebrew!" << std::endl;
             std::cout << "Please use --compile-info flag for information about the comile." << std::endl;
       }
 
-      if (argc == 0)
+      if (argc == 2)
       {
-            if (argv[0] == "--compile-info")
+            if (argv[1] == "--compile-info")
             {
                   std::cout << "Compiled on: " << __DATE__ << std::endl;
                   std::cout << "Compiled at: " << __TIME__ << std::endl;
             }
 
-            if (argv[0] == "--version")
+            if (argv[1] == "--version")
             {
                   std::cout << VERSION << std::endl;
             }
